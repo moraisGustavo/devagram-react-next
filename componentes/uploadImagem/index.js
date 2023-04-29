df7d123e3e9e8ef381923a965414261493382499
Gustavo Morais <gustavohms.santos@gmail.com>
2023-04-28 23:25:42 -0300

    Componentes de Botao , avatar , upload de Imagem

---
+++ componentes/uploadImagem/index.js
@@ -0,0 +1,62 @@
+import { useEffect, useRef } from "react";
+
+export function UploadImagem ({
+    className ='',
+    setImagem,
+    imagemPreview,
+    imagemPreviewClassName ='',
+    aoSetarAreferencia
+}) {
+    const refereciaInput = useRef(null);
+
+    useEffect(() => {
+        if(!aoSetarAreferencia){
+            return;
+        }
+
+        aoSetarAreferencia(refereciaInput?.current);
+    }, [refereciaInput?.current]);
+
+    const abrirSeletorArquivos =() => {
+        refereciaInput?.current?.click();
+    }
+
+    const aoAlterarImagem = () => {
+        if(!refereciaInput?.current?.files?.length){
+            return;
+        }
+         
+        const arquivo = refereciaInput?.current?.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(arquivo);
+        fileReader.onloadend = () => {
+            setImagem({
+                preview: fileReader.result,
+                arquivo
+            });
+        }
+    }
+
+
+    return (
+        <div className={`uploadImagemContainer ${className}`} onClick={abrirSeletorArquivos}>
+            {imagemPreview && (
+                <div className="imagemPreviewContainer">
+                    <img
+                        src={imagemPreview}
+                        alt='Imagem Preview'
+                        className={imagemPreviewClassName}
+                    />
+
+                </div>
+            )}
+            <input
+                type='file'
+                className='oculto' 
+                accept="image/*"
+                ref={refereciaInput}
+                onChange={aoAlterarImagem}
+             />
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { UploadImagem } from '../componentes/uploadImagem'
+import { useRef, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+  const refereciaInput = useRef(null);
+
+  console.log(imagem);
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <button onClick={() => refereciaInput?.current?.click()}> abrir seletor de arquivos</button>
+
+      <UploadImagem 
+        setImagem={setImagem} 
+        imagemPreview={imagem?.preview} 
+        aoSetarAreferencia={(ref) => refereciaInput.current = ref}
+      />
+    </>
   )
 }
