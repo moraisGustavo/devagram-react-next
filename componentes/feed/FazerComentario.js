a42aa560bebe3880c875eb0baf7e16c499c68616
Gustavo Morais <gustavohms.santos@gmail.com>
2023-05-11 20:26:37 -0300

    Revert "feed resolvido"
    This reverts commit 00728988500f764dbc2e44a59b826314647cdb37.

--- componentes/feed/FazerComentario.js
+++ componentes/feed/FazerComentario.js
@@ -7,7 +7,7 @@ export function FazerComentario({usuarioLogado}) {
             <textarea placeholder="Adicione um comentario..." rows={1}>
             </textarea>
 
-            <button type="button" className="btnPublicacao desktop">
+            <button type="button" className="btnPublicacao Desktop">
                 Publicar
             </button>
         </div>
--- componentes/feed/index.js
+++ componentes/feed/index.js
@@ -7,56 +7,32 @@ const feedService = new FeedService();
 export function Feed({ usuarioLogado }) {
     const [listaDePostagens, setListaDePostagens] = useState([]);
 
-
-    // TESTE 1 
     // useEffect(() => {
     //     async function carregarPostagens() {
     //         const { data } = await feedService.carregarPostagens();
     //         console.log(data);
     //     }
-    //     setListaDePostagens([
+
+    //     const postagensFormatadas = data.map((postagem) => (
     //         {
-    //             id: '1',
+    //             id: postagem._id,
     //             usuario: {
-    //                 id: '1',
-    //                 nome: 'Gustavo',
-    //                 avatar: null,
+    //                 id: postagem.userId,
+    //                 nome: postagem.usuario.nome,
+    //                 avatar: postagem.usuario.avatar
     //             },
-    //             fotoDoPost: 'https://i.pinimg.com/564x/b5/60/cb/b560cbee05d252544e66c11a9129be08.jpg',
-    //             descricao: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere iste id, labore repellat dolores explicabo libero cumque nemo sit in enim accusantium ullam corporis autem illo at, ad amet fugiat.',
-    //             likes: [],
-    //             comentarios: [
-    //                 {
-    //                     nome: 'Fulano',
-    //                     mensagem: 'Comentario 1'
-    //                 },
-    //                 {
-    //                     nome: 'Ciclano',
-    //                     mensagem: 'Comentario 2'
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             id: '2',
-    //             usuario: {
-    //                 id: '2',
-    //                 nome: 'Beltrano',
-    //                 avatar: null,
-    //             },
-    //             fotoDoPost: 'https://i.pinimg.com/564x/d5/b8/af/d5b8af8ac9e47f7094fa09127f56fd87.jpg',
-    //             descricao: 'Nice!',
-    //             likes: [],
-    //             comentarios: [{
-    //                 nome: 'Ciclano',
-    //                 mensagem: 'Comentario 3'
-    //             }]
-    //         },
-    //     ]);
-    // }, [usuarioLogado]);
+    //             fotoDoPost: postagem.foto,
+    //             descriçao: postagem.descricao,
+    //             curtidas: postagem.likes,
+    //             comentarios: postagem.comentarrios.map(c => ({
+    //                 nome: c.nome,
+    //                 mensagem: c.comentario
+    //             }))
+    //         }
+    //     ));
 
+    //     setListaDePostagens(postagensFormatadas);
 
-
-    // TESTE 2
 
     useEffect(() => {
         async function carregarPostagens() {
@@ -65,16 +41,16 @@ export function Feed({ usuarioLogado }) {
             const postagensFormatadas = data.map((postagem) => ({
                 id: postagem._id,
                 usuario: {
-                    id: postagem.idUsuario,
+                    id: postagem.userId,
                     nome: postagem.usuario.nome,
                     avatar: postagem.usuario.avatar,
                 },
                 fotoDoPost: postagem.foto,
-                descricao: postagem.descricao,
+                descriçao: postagem.descricao,
                 curtidas: postagem.likes,
                 comentarios: postagem.comentarios.map((c) => ({
                     nome: c.nome,
-                    mensagem: c.comentario,
+                    mensagem: c.comentarios,
                 })),
             }));
             setListaDePostagens(postagensFormatadas);
@@ -83,9 +59,46 @@ export function Feed({ usuarioLogado }) {
         carregarPostagens();
     }, [usuarioLogado]);
 
-    
 
-
+    //     setListaDePostagens([
+    //         {
+    //             id: '1',
+    //             usuario: {
+    //                 id: '1',
+    //                 nome: 'Sauron',
+    //                 avatar: null,
+    //             },
+    //             fotoDoPost: 'https://i.pinimg.com/564x/b5/60/cb/b560cbee05d252544e66c11a9129be08.jpg',
+    //             descricao: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere iste id, labore repellat dolores explicabo libero cumque nemo sit in enim accusantium ullam corporis autem illo at, ad amet fugiat.',
+    //             likes: [],
+    //             comentarios: [
+    //                 {
+    //                     nome: 'Orc',
+    //                     mensagem: 'HOEKSMCO'
+    //                 },
+    //                 {
+    //                     nome: 'Orc2',
+    //                     mensagem: 'OXIAHJPXI'
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             id: '2',
+    //             usuario: {
+    //                 id: '2',
+    //                 nome: 'Gandalf',
+    //                 avatar: null,
+    //             },
+    //             fotoDoPost: 'https://i.pinimg.com/564x/d5/b8/af/d5b8af8ac9e47f7094fa09127f56fd87.jpg',
+    //             descricao: 'Nice!',
+    //             likes: [],
+    //             comentarios: [{
+    //                 nome: 'Ciclano',
+    //                 mensagem: 'Gandalf The lord of the Rings'
+    //             }]
+    //         },
+    //     ]);
+    // }, [usuarioLogado]);
 
     return (
         <div className="feedContainer largura50pctDescktop">
